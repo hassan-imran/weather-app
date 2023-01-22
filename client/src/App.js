f48aab43209aace81cc49b292b9504d5a4f51c48
Hassan Imran <43490649+hassan-imran@users.noreply.github.com>
2023-01-22 17:10:52 +0500

    Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 // import logo from './logo.svg';
 // import './App.css';
 import socketIO from 'socket.io-client';
+import SignIn from './routes/SignIn';
+import { Routes, Route, Navigate, Link } from "react-router-dom";
+import SignUp from './routes/SignUp';
+
 const socket = socketIO.connect('http://localhost:8000');
 
 function App() {
 
   return (
     <div className="App">
-      Hello World
+
+      {/* <SignIn /> */}
+
+      <Routes>
+        <Route path='/' element={<SignIn />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        {/* <Route path='/signin' element={<SignIn />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signin' element={<SignIn />} /> */}
+      </Routes>
+
     </div>
   );
 }
